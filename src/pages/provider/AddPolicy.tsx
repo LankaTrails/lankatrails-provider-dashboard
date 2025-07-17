49f09932501d7a96c2a5b232a69e1fce82230deb
sakiththewmika <sakiththewmika@gmail.com>
2025-07-17 17:29:53 +0530

    [LT-24] Add routes for policy management

---
+++ src/pages/provider/AddPolicy.tsx
@@ -0,0 +1,14 @@
+import ProviderTopBar from "@/components/provider/ProviderTopBar";
+
+const AddPolicy = () => {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h1 className="text-2xl p-2 font-bold">Add policies</h1>
+        <ProviderTopBar />
+      </div>
+    </div>
+  );
+};
+
+export default AddPolicy;
